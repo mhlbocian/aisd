7d90da10622c7419dffb9d0ec7a1c15f530c8fc1
EwaFengler <ewa.fen@interia.pl>
2017-04-22 20:14:23 +0200

    zaczęte! :P

---
+++ grafowe/includes.h
@@ -0,0 +1,41 @@
+/**
+ * POLITECHNIKA POZNANSKA - WYDZIAL INFORMATYKI - INFORMATYKA I
+ * ALGORYTMY I STRUKTURY DANYCH
+ *
+ * Algorytmy grafowe - DFS, min. drzewo rozpinajace
+ *
+ * Autorzy: Michal Bocian, Ewa Fengler
+ * Rok akademicki: 2016/2017
+ */
+
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+typedef struct{
+    int value;
+    list_item *next;
+} item;
+
+typedef struct{
+    int value;
+    node *left;
+    node *right;
+} node;
+
+/*generowanie DAGu*/
+//void genDAGmatrix(tablica, int rozm);
+//void genDAGlist(tablica z wsk do list, tablica z grafem, int rozm);
+
+/*generowanie grafu nieskierowanego z wagami*/
+//void genGraphMatrix(tablica, int nasycenie, int rozm);
+//void genGraphList(tablica z wsk do list, tablica z grafem, int rozm);
+
+/*sortowanie topologiczne*/
+//item* topologicalSortMatrix(tablica, int rozm);
+//item* topologicalSortList(tablica z wsk do list, int rozm);
+
+/*min drzewo rozpinajace*/
+//node* MST_matrix(tablica, int rozm);
+//node* MST_list(tablica z wsk do list, int rozm);
+
+#endif
