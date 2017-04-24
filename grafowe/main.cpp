2f6e3be8c1007e7f0679970815b2c8332e400a3f
EwaFengler <ewa.fen@interia.pl>
2017-04-24 21:56:25 +0200

    poprawki teraz juz prawie dziala

--- grafowe/main.cpp
+++ grafowe/main.cpp
@@ -23,18 +23,18 @@ struct List{
 	List* next;
 };
 
-struct sList{ //do listy topologicznej
+struct slist{ //do listy topologicznej
     int num;
     slist *next;
 };
 
 int** createMatrix(int n){
 	int** matrix = new int*[n];
-	
+
 	for (int i = 0; i < n; i++){
 		matrix[i] = new int[n];
 	}
-	
+
 	return matrix;
 }
 
@@ -57,7 +57,7 @@ void listInsert(List*& item, int value, int num){
 
 void listArrayDelete(List** list, int size){
 	List* temp;
-	
+
 	for (int i = 0; i < size; i++){
 		while (list[i]){
 			temp = list[i];
@@ -65,14 +65,14 @@ void listArrayDelete(List** list, int size){
 			delete temp;
 		}
 	}
-	
+
 	delete list;
 }
 
 void genDAGmatrix(int** tab, int rozm){
     int i, j;
     int numOfVertices = rozm * (rozm - 1) * 0.5 * 0.6;
-    
+
     for (i = 0; i < rozm; i++){
         for (j = 0; j < rozm; j++){
             tab[i][j] = 0;
@@ -114,7 +114,7 @@ List* topologicalSortMatrix(int* tab, int rozm){
 void genGraphMatrix(int **tab, float nasycenie, int rozm){
     int i, j, val;
     int numOfVertices = rozm * (rozm - 1) * 0.5 * nasycenie;
-    
+
     for (i = 0; i < rozm; i++){
         for (j = 0; j < rozm; j++){
             tab[i][j] = 0;
@@ -153,6 +153,26 @@ void genGraphList(List** listArray, int **tab, int rozm){
     }
 }
 
+
+slist DFSVisit(int *color, int *tab, int i, slist element, int rozm){ // TODO: ze tablica 2D
+    //wierzcholek odwiedzony
+    color[i] = gray;
+
+    for (int j = i+1; j < rozm; j++){
+        if (tab[i][j]){
+            if(color[j] == white){
+                DFSVisit (color, tab, j, element, rozm);
+            }
+        }
+    }
+    //wierzcholek przetworzony
+    color[i] = black;
+
+    //tworzymy liste wynikowa, posortowana topologicznie
+    slist newElem = {i, &element};
+    return newElem;
+}
+
 slist topologicalSortMatrix (int *colors, int *tab, int rozm){// TODO: ze tablica 2D
     slist headTopological = {-1, NULL};
 
@@ -161,41 +181,22 @@ slist topologicalSortMatrix (int *colors, int *tab, int rozm){// TODO: ze tablic
     }
 
     for (int i = 0; i < rozm; i++){
-            if (colors[i] == white){
-                    headTopological = DFS_visit(tab, i, headTopological, rozm);
-            }
+        if (colors[i] == white){
+            headTopological = DFSVisit(colors, tab, i, headTopological, rozm);
         }
     }
     return headTopological;
 }
 
-slist DFSVisit(int *tab, int i, slist element, int rozm){ // TODO: ze tablica 2D
-    //wierzcholek odwiedzony
-    color[i] = gray;
-
-    for (int j = i+1; j < rozm; j++){
-        if (tab[i][j]){
-            if(color[j] == white){
-                DFS_visit (tab, j, rozm);
-            }
-        }
-    }
-    //wierzcholek przetworzony
-    color[i] = black;
-
-    //tworzymy liste wynikowa, posortowana topologicznie
-    slist newElem = {i, element};
-    return newElem;
-}
 
 
 int main(void){
 	srand(time(NULL));
-	
+
 	int i, inc, n, start, stop;
 	int** matrix;
 	List** list;
-	
+
 	cout<<"Poczatek pomiaru: ";
 	cin>>start;
 	cout<<"Koniec pomiaru: ";
@@ -203,21 +204,21 @@ int main(void){
 	cout<<"Ilosc krokow: ";
 	cin>>n;
 	inc = (stop - start) / n;
-	
+
 	for(i = 0; i < n; i++){
 		// stop to aktualna proba
 		stop = start + i * inc;
 		list = new List*[stop];
-		
+
 		cout<<stop<<":";
 		matrix = createMatrix(stop);
 		genDAGmatrix(matrix, stop);
 		genDAGlist(list, matrix, stop);
-		
+
 		deleteMatrix(matrix, stop);
 		listArrayDelete(list, stop);
 		cout<<endl;
 	}
-	
+
 	return 0;
 }
