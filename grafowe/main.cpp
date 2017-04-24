f9a164024b81146e7dea76995187eaac0458858c
Michal Bocian <bocian.michal@outlook.com>
2017-04-24 20:55:08 +0200

    punkt zero

--- grafowe/main.cpp
+++ grafowe/main.cpp
@@ -8,6 +8,8 @@
  * Rok akademicki: 2016/2017
  */
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 
 using namespace std;
 
@@ -41,6 +43,94 @@ void list_insert(List*& item, int value, int nedge){
         item->next = NULL;
     } else {
         list_insert(item->next, value, nedge);
+    }
+}
+
+void genDAGmatrix(int** tab, int rozm){
+    int i, j;
+    int numOfVertices = rozm * (rozm - 1) * 0.5 * 0.6;
+    
+    for (i = 0; i < rozm; i++){
+        for (j = 0; j < rozm; j++){
+            tab[i][j] = 0;
+        }
+    }
+
+    while (numOfVertices){
+        i = rand() % rozm;
+        j = rand() % (rozm - i) + i;
+
+        if(tab[i][j] == 0){
+            tab[i][j] = 1;
+            numOfVertices--;
+        }
+    }
+    //2 - ew. przemieszanie
+}
+
+
+void genDAGlist(List** listArray, int **tab, int rozm){
+    int i, j;
+
+    for (i = 0; i < rozm; i++){
+        listArray[i] = NULL;
+    }
+
+    for (i = 0; i < rozm; i++){
+        for (j = i + 1; j < rozm; j++){
+            if(tab[i][j]){
+                list_insert(listArray[i], 0, j);
+            }
+        }
+    }
+}
+
+List* topologicalSortMatrix(int* tab, int rozm){
+    return NULL;
+}
+
+
+
+void genGraphMatrix(int **tab, float nasycenie, int rozm){
+    int i, j, val;
+    
+    for (i = 0; i < rozm; i++){
+        for (j = 0; j < rozm; j++){
+            tab[i][j] = 0;
+        }
+    }
+
+    int numOfVertices = rozm * (rozm - 1) * 0.5 * nasycenie;
+
+    while (numOfVertices){
+        i = rand() % rozm;
+        j = rand() % rozm;
+
+        if(tab[i][j] == 0){
+            val = rand() % 1000 + 1;
+            tab[i][j] = val;
+            tab[j][i] = val;
+            numOfVertices--;
+        }
+    }
+}
+
+void genGraphList(List** listArray, int **tab, int rozm){
+    int i, j;
+
+    for (i = 0; i < rozm; i++){
+        listArray[i] = NULL;
+    }
+
+    for (i = 0; i < rozm; i++){
+        for (j = i + 1; j < rozm; j++){
+            if(tab[i][j]){
+                list_insert(listArray[i], j, tab[i][j]);
+                list_insert(listArray[j], i, tab[i][j]);
+                tab[i][j] = 0;
+                tab[j][i] = 0;
+            }
+        }
     }
 }
 
